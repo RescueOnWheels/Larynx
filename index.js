84bcdf8f7144c88f3cf982efa3a5294f0d783d34
Martijn Vegter <mail@mvegter.me>
2018-09-13 11:43:46 +0200

    whatthecommit.com’s server IP address could not be found.
    Added basic communication layout.

---
+++ index.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events').EventEmitter;
+const io = require('socket.io-client');
+
+class Larynx extends EventEmitter {
+    constructor() {
+        super();
+    }
+
+    Connect() {
+        this.socket = io.connect('http://127.0.0.1:3000');
+
+        this.socket.on('connect', () => {
+            console.log("Connected");
+        });
+
+        this.socket.on('disconnect', () => {
+            console.log("Disconnected");
+        });
+
+        this.socket.on('move', (data) => {
+            this.emit('move', data);
+        })
+    }
+
+    Disconnect() {
+        this.socket.disconnect();
+        this.socket = null;
+    }
+}
+
+
+module.exports = Larynx;
