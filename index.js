6557049b91b6180d4fdfc3d3e8fc32915808e764
Martijn Vegter <martijn@martijnvegter.com>
2018-09-18 20:14:47 +0200

    Derp, helper method rename
    Potential fix for `disconnect` event.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-//Very ugly I know very WIP
+const DEBUG = false;
+
+/* Start of IP screen */
+
 const ip = require('ip');
 const LCD = require('lcdi2c');
 
@@ -8,29 +11,70 @@ const ip_addr = ip.address();
 lcd.clear();
 lcd.print(ip_addr);
 
-const EventEmitter = require('events').EventEmitter;
-const io = require('socket.io-client');
+/* Start of program */
 
-function Larynx() {
-    this.emitter = new EventEmitter();
-}
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
 
-Larynx.prototype.Connect = () => {
-        this.socket = io.connect('http://127.0.0.1:3000');
+const io = require('socket.io')();
+const token = 'ABXY';
 
-        this.socket.on('connect', () => {
-            console.log("It's alive!");
-        });
+io.on('connection', (socket) => {
+    /* Authentication */
+    socket.emit('authenticate', (data) => {
+        if (data !== token) {
+            socket.disconnect();
+            return;
+        }
 
-        this.socket.on('disconnect', () => {
-            console.log("Disconnected");
-            this.emit('disconnect');
-        });
+        socket.cockpit = true;
+        socket.emit('authenticated');
+    });
 
-        this.socket.on('move', (data) => {
-            this.emit('move', data);
-        });
-    };
+    /* Heartbeat */
 
+    let isStoppped = false;
+    let lastBeat = Date.now();
 
-module.exports = Larynx;
+    setInterval(() => {
+        if (lastBeat + 1000 < Date.now()) {
+            if (isStoppped) {
+                return;
+            }
+
+            isStoppped = true;
+            Stop();
+        }
+    }, 100);
+
+    socket.emit('heartbeat');
+    socket.on('heartbeat', function() {
+        if (DEBUG) console.log("heart");
+
+        lastBeat = Date.now();
+        socket.emit('heartbeat');
+    });
+
+    socket.on('disconnect', () => {
+        Stop();
+    });
+
+    function Stop() {
+        if (DEBUG) console.log("SOS");
+        myEmitter.emit('disconnect');
+    }
+
+    /* Data Comms */
+    socket.on('move', (data) => {
+        if (!socket.cockpit) {
+            return;
+        }
+
+        myEmitter.emit('move', data);
+    });
+});
+
+io.listen(3000);
+
+module.exports = myEmitter;
