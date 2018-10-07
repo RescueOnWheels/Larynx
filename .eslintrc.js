caefe0f8ac394c4e6b9dcc9c138d1c13055934ae
Martijn Vegter <martijn@martijnvegter.com>
2018-10-07 14:31:42 +0200

    For great justice. (#10)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-    "env": {
-        "mocha": true,
-        "node": true,
-    },
-    "extends": "airbnb-base",
-    "rules": {
-        "no-console": ["error", {
-            "allow": ["debug", "error", "info", "warn"]
-        }],
-        "no-multiple-empty-lines": ["error", {
-            "max": 1,
-            "maxBOF": 0,
-            "maxEOF": 1
-        }],
-    }
+  "env": {
+    "mocha": true,
+    "node": true,
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "no-multiple-empty-lines": ["error", {
+      "max": 1,
+      "maxBOF": 0,
+      "maxEOF": 1
+    }],
+  }
 };
