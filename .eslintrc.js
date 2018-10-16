7dcd59bfd5ca4c49564b8dae4e0138538102ce88
Martijn Vegter <mail@mvegter.me>
2018-10-16 13:36:48 +0200

    It's time to go home. (#22)
    * WIP
    
    * Added controller event.
    
    * Added headset to larynx
    
    * Updated beat

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,17 @@ module.exports = {
   },
   "extends": "airbnb-base",
   "rules": {
+    "multiline-comment-style": ["error", "starred-block"],
     "no-multiple-empty-lines": ["error", {
       "max": 1,
       "maxBOF": 0,
       "maxEOF": 1
     }],
+    "no-param-reassign": "off",
+    "spaced-comment": ["error", "always", {
+      "block": {
+        "balanced": true
+      }
+    }]
   }
 };
