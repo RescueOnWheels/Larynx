1981d12f11cdf7ca2a54a9e1b44374bed7292cc1
Martijn Vegter <mail@mvegter.me>
2018-11-22 16:03:37 +0100

    This really should not take 19 minutes to build.

--- test/mock/index.js
+++ test/mock/index.js
@@ -1,8 +1,15 @@
 /* Helpers */
 const generateToken = require('./helpers/generateToken.test');
 
+/* Lib */
+const Auxilio = require('./../../lib/Auxilio/test');
+
 describe('Mock', () => {
   describe('helpers', () => {
     describe('generateToken', generateToken);
+  });
+
+  describe('lib', () => {
+    describe('Auxilio', Auxilio);
   });
 });
